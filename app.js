d497b9da131aa9e957652dd4381dfdea629291a3
Victor Garritano <victor@getupgraded.com>
2024-10-23 23:54:15 +0200

    Terminado miercoles 23oct

--- app.js
+++ app.js
@@ -11,39 +11,6 @@ app.get("/", (req, res) => {
     res.send("Funcionando");
 });
 
-// // GET http://localhost:3000/films
-// app.get("/films", (req, res) => {
-//   res.send("Aquí van tus pelis");
-// });
-
-// // GET http://localhost:3000/all
-// // GET http://localhost:3000/perros/mordisquitos
-// // GET http://localhost:3000/perros/bolita
-// // GET http://localhost:3000/perros/23
-// // GET http://localhost:3000/perros --> devuelve todos los perros
-
-// app.get("/perros/:name?", (req, res) => {
-//   // ? indica que el parametro es opcional
-//   const name = req.params.name; // leer el parametro name
-//   //Habría que sustitur las siguientes líneas (28-35) por una llamada a mi BBDD SQL
-//   // select * from perros where name = name
-//   //perros.model.getPerrosByName(name)
-//   const perros = [
-//     { name: "mordisquitos", age: 2 },
-//     { name: "toby", age: 3 },
-//     { name: "peluson", age: 5 },
-//     { name: "bob", age: 3 },
-//   ];
-//   if (name) {
-//     const perro = perros.find((perro) => perro.name === name);
-
-//     perro // perro encontrado????
-//       ? res.status(200).json(perro) // si lo encuentra, devuelvo el perro
-//       : res.status(404).json({ message: name + " no encontrado" }); // sino, objeto con un mensaje
-//   } else {
-//     res.status(200).json(perros); // devuelve todos los perros
-//   }
-// });
 
 // HTTP GET http://localhost:3000/all
 app.get("/all", (req, res) => {
@@ -130,7 +97,8 @@ app.get("/country/at-least/germany", (req, res) => {
     })
 });
 
-//Crea una ruta /pages/all-greater/200 para obtener VERDADERO O FALSO dependiendo de si todos los libros tienen más de 200 páginas
+//Crea una ruta /pages/all-greater/200 para obtener VERDADERO O FALSO dependiendo de si todos los libros tienen 
+//más de 200 páginas
 app.get("/pages/all-greater/200", (req, res) => {
     books.forEach(book => {
         if (book.pages < 200) {
@@ -142,69 +110,6 @@ app.get("/pages/all-greater/200", (req, res) => {
     })
 })
 
-
-
-
-
-
-
-// //HTTP POST http://localhost:3000/books
-// app.post("/books", (req, res) => {
-//   console.log(req.body);
-//   if (req.body.title && req.body.author) {
-//     // Lógica para guardar el libro en la BBDD
-//     // INSERT INTO books (title, author) VALUES (req.body.title, req.body.author)
-//     //..
-//     res.status(201).json({
-//       success: true,
-//       action: "create",
-//       title: req.body.title,
-//       id: Math.floor(Math.random() * (10000 - 1) + 1),
-//       data: req.body,
-//     });
-//   } else {
-//     res.status(400).send("Petición incorrecta");
-//   }
-// });
-
-// //HTTP PUT http://localhost:3000/books
-// app.put("/books", (req, res) => {
-//   console.log(req.body); // por body se recibe el libro a editar
-//   if (req.body.title && req.body.author) {
-//     // Lógica para editar el libro en la BBDD
-//     // UPDATE books SET title = req.body.title, author = req.body.author WHERE id = req.body.id
-//     //..
-
-//     let book = {
-//       "title": "Don Quijote de la Mancha",
-//       "author":"Miguel de Cervantes",
-//       "pages": 2000,
-//       "year":1550,
-//       "description": "en un lugar de la mancha..."
-//     }
-
-//     let newBook = {...book, ...req.body}; // Actualizar el libro con los nuevos datos
-
-//     res.status(200).json({
-//       success: true,
-//       action:"update",
-//       title: req.body.title,
-//       id: Math.floor(Math.random() * (10000 - 1) + 1),
-//       data: newBook,
-//     });
-//   } else {
-//     res.status(400).send("Petición incorrecta");
-//   }
-// });
-
-// //HTTP DELETE http://localhost:3000/books?title=Hamlet&author=Shakespeare --> Borrar un libro
-// //HTTP DELETE http://localhost:3000/books --> Borrar todos los libros
-// app.delete("/books", (req, res) => {
-//   console.log(req.query);
-//   // Lógica para borrar de la bbdd por título y autor
-//   // DELETE FROM books WHERE title = req.query.title AND author = req.query.author
-//   res.send(`Libro borrado: ${req.query.title} - ${req.query.author}`);
-// });
 
 // Para ruta no existente
 app.use("*", (req, res) => {
